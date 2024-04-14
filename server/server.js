9be140e4fddfaf0991ccc5bbf95d035c98a6f696
Jugeh <jukkasalokoski1@gmail.com>
2024-04-15 02:18:07 +0300

    Initial commit

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const mongo_url = process.env.MONGODB_URL;
 const mongo_user = process.env.MONGODB_USER;
 const mongo_password = process.env.MONGODB_PASSWORD;
 
-const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"?retryWrites=true&w=majority&appName=testi";
+const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"test?retryWrites=true&w=majority&appName=testi";
 
 mongoose.connect(url)
   .then(() => console.log('MongoDB connection successful'))
