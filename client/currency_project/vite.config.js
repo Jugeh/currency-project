1ce1f887666c9f3255582a359ca384b4d051e8b6
Jugeh <jukkasalokoski1@gmail.com>
2024-04-15 08:17:37 +0300

    Initial commit

--- client/currency_project/vite.config.js
+++ client/currency_project/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/{currency-project}",
-  plugins: [react()]
+  plugins: [react()],
 })
