4c1afd7fd277f7573c0dd98ad40a0ba343dcbca6
Jugeh <jukkasalokoski1@gmail.com>
2024-04-16 21:06:14 +0300

    Initial commit

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.get('/rates', (req, res) => {
 
 app.get('*', (req, res) => {
   console.log(`Current directory: ${process.cwd()}`);
-  const filePath = path.join(process.cwd(), '..', 'client', 'currency_project', 'dist', 'index.html');
+  const filePath = path.join(process.cwd(), 'client', 'currency_project', 'dist', 'index.html');
   console.log(`File path: ${filePath}`);
   res.sendFile(filePath);
 });
